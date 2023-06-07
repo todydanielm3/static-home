c720fac9a162cdd65a6c5e24b59e104418350f7d
Daniel Moraes <36305237+todydanielm3@users.noreply.github.com>
2023-06-07 14:28:35 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,13 @@ import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../lib/theme'
 
-
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
+        <Head />
         <body>
+          {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
@@ -16,3 +17,4 @@ export default class Document extends NextDocument {
     )
   }
 }
+
